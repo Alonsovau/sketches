0684ba59b7994fd91adf536bd45497a05e7368ef
alonsovau <alonsovau@outlook.com>
2017-06-05 00:08:31 +0800

    2017-6-4

---
+++ chapter8/st1.py
@@ -0,0 +1,16 @@
+# 改变对象的字符串显示
+class Pair:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Pair({0.x!r}, {0.y!r})'.format(self)
+
+    def __str__(self):
+        return '({0.x!s}, {0.y!s})'.format(self)
+
+
+p = Pair(3, 4)
+print(p.__repr__())
+print(p)
---
+++ chapter8/st2.py
@@ -0,0 +1,34 @@
+# 自定义字符串的格式化
+_formats = {
+    'ymd': '{d.year}-{d.month}-{d.day}',
+    'mdy': '{d.month}/{d.day}/{d.year}',
+    'dmy': '{d.day}/{d.month}/{d.year}'
+
+}
+
+
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def __format__(self, format_spec):
+        if format_spec =='':
+            format_spec = 'ymd'
+        fmt = _formats[format_spec]
+        return fmt.format(d=self)
+
+
+d = Date(2012, 12, 21)
+print(format(d))
+print(format(d, 'mdy'))
+print('The date is {:ymd}'.format(d))
+print('The date is {:mdy}'.format(d))
+
+
+from datetime import date
+d = date(2012, 12, 21)
+print(format(d))
+print(format(d, '%A, %B %d, %Y'))
+print('The end is {:%d %b %Y}. Goodbye'.format(d))
