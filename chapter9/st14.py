d11ee06f8964740e34a6022090417ec11e1fab44
alonsovau <alonsovau@outlook.com>
2017-06-25 01:05:53 +0800

    2017-6-25

--- chapter9/st14.py
+++ chapter9/st14.py
@@ -22,6 +22,10 @@ class Float(Typed):
     _expected_type = float
 
 
+class String(Typed):
+    _expected_type = str
+
+
 class OrderedMeta(Typed):
     def __new__(cls, clsname, bases, clsdict):
         d = dict(clsdict)
@@ -36,3 +40,24 @@ class OrderedMeta(Typed):
     @classmethod
     def __prepare__(cls, clsname, bases):
         return OrderedDict()
+
+
+class Structure(metaclass=OrderedMeta):
+    def as_csv(self):
+        return ','.join(str(getattr(self, name)) for name in self._order)
+
+
+class Stock(Structure):
+    name = String()
+    shares = Integer()
+    price = Float()
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+
+s = Stock('GOOD', 100, 490.1)
+print(s.name)
+
