feb85b082f90ea0aca77f33d998255edfde980c7
alonsovau <alonsovau@outlook.com>
2017-07-05 22:08:06 +0800

    2017-7-5

--- test9.py
+++ test9.py
@@ -1,4 +1,4 @@
-import re, datetime
+import re, time
 
 
 class ss:
@@ -9,18 +9,24 @@ def write(result):
     with open('result.txt', 'w') as f:
         for key, value in result.items():
             f.write(key+'\n')
-            # sorted(value.items(), key=lambda d: d[1])
-            for key2, value2 in value.items():
-                f.write('\t\t'+key2+'------'+str(value2)+'\n')
+            value = sorted(zip(value.values(), value.keys()))
+            f.write(lambda t : value[:])
 
 
 def indate(start_date, end_date, date):
-    pass
+    if start_date == '':
+        return True
+    if end_date == '':
+        end_date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+    if len(start_date) != 8 or len(end_date) != 8:
+        raise TypeError('date format:20170707')
+    if start_date <= date <= end_date:
+        return True
+    return False
 
 if __name__ == '__main__':
     start_date = input('start_date: ')
     end_date = input('end_date: ')
-    sdate = datetime()
     datepat = re.compile(r'\d{4}-\d{1,2}-\d{1,2}')
     urlpat = re.compile('\S*\.(?!\d+)[a-zA-Z0-9]+\.[a-z A-Z]*')
     hostpat = re.compile('\d*\.\d*\.\d*\.\d*')
@@ -29,19 +35,20 @@ if __name__ == '__main__':
         result = {}
         for line in lines[:]:
             if datepat.findall(line):
-                # print(line)
-                if re.findall('INFO', line):
-                    if urlpat.findall(line):
-                        url = urlpat.findall(line)[0]
-                        # print(url)
-                        host = hostpat.findall(line)[0]
-                        # print(host)
-                        if host not in result:
-                            result[host] = {url: 1}
-                        elif url not in result[host]:
-                            result[host][url] = 1
-                        else:
-                            result[host][url] += 1
+                if indate(start_date, end_date, datepat.findall(line)[0]):
+                    # print(line)
+                    if re.findall('INFO', line):
+                        if urlpat.findall(line):
+                            url = urlpat.findall(line)[0]
+                            # print(url)
+                            host = hostpat.findall(line)[0]
+                            # print(host)
+                            if host not in result:
+                                result[host] = {url: 1}
+                            elif url not in result[host]:
+                                result[host][url] = 1
+                            else:
+                                result[host][url] += 1
         write(result)
 
 
