931e069bde4b252e7641c849ee0038427e982147
alonsovau <alonsovau@outlook.com>
2017-07-18 00:17:35 +0800

    2017-7-17

--- chapter11/st11s.py
+++ chapter11/st11s.py
@@ -1,6 +1,30 @@
 from multiprocessing.connection import Listener
 from multiprocessing.reduction import send_handle
 import socket, sys
+import struct
+
+
+def send_fd(sock, fd):
+    sock.sendmsg([b'x'],
+                 [(socket.SOL_SOCKET, socket.SCM_RIGHTS, struct.pack('i', fd))])
+    ack = sock.recv(2)
+    assert ack == b'OK'
+
+
+def server2(work_address, port):
+    work_recv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    work_recv.bind(work_address)
+    work_recv.listen(1)
+    worker, addr = work_recv.accept()
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    s.bind(('', port))
+    s.listen(1)
+    while True:
+        client, addr = s.accept()
+        print('SERVER: Got connection from ', addr)
+        send_fd(worker, client.fileno())
+        client.close()
 
 
 def server(work_address, port):
@@ -22,4 +46,5 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: server.py server_port port', file=sys.stderr)
         raise SystemExit(1)
-    server(sys.argv[1], int(sys.argv[2]))
+    # server(sys.argv[1], int(sys.argv[2]))
+    server2((sys.argv[1], int(sys.argv[2])))
--- chapter11/st11w.py
+++ chapter11/st11w.py
@@ -2,6 +2,29 @@ from multiprocessing.connection import Client
 from multiprocessing.reduction import recv_handle
 import os, sys
 from socket import socket, AF_INET, SOCK_STREAM
+import socket as SOCKET
+import struct
+
+
+def recv_fd(sock):
+    msg, ancdata, flags, addr = sock.recvmsg(1, SOCKET.CMSG_LEN(struct.calcsize('i')))
+    cmsg_level, cmsg_type, cmsg_data = ancdata[0]
+    assert cmsg_level == SOCKET.SOL_SOCKET and cmsg_type == SOCKET.SCM_RIGHTS
+
+
+def worker2(server_address):
+    serv = socket(SOCKET.AF_UNIX, SOCKET.SOCK_STREAM)
+    serv.connect(server_address)
+    while True:
+        fd = recv_fd(serv)
+        print('WORKER: Got FD', fd)
+        with socket(SOCKET.AF_INET, SOCKET.SOCK_STREAM, fileno=fd) as client:
+            while True:
+                msg = client.recv(1024)
+                if not msg:
+                    break
+                print('WORKER: RECV {!r}'.format(msg))
+                client.send(msg)
 
 
 def worker(server_address):
@@ -24,4 +47,5 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('Usage: worker.py server_address', file=sys.stderr)
         raise SystemExit(1)
-    worker((sys.argv[1]))
+    # worker((sys.argv[1]))
+    worker(sys.argv[1])
