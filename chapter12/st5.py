f0e9d37d4215606efb4abfe6b13dbda17a6870a8
alonsovau <alonsovau@outlook.com>
2017-08-06 23:43:42 +0800

    2017-8-6

--- chapter12/st5.py
+++ chapter12/st5.py
@@ -28,16 +28,30 @@ x_lock = threading.Lock()
 y_lock = threading.Lock()
 
 
+# def thread_1():
+#     while True:
+#         with acquire(x_lock, y_lock):
+#             print('Thread-1')
+#
+#
+# def thread_2():
+#     while True:
+#         with acquire(y_lock, x_lock):
+#             print('Thread-2')
+
+
 def thread_1():
     while True:
-        with acquire(x_lock, y_lock):
-            print('Thread-1')
+        with acquire(x_lock):
+            with acquire(y_lock):
+                print('Thread-1')
 
 
 def thread_2():
     while True:
-        with acquire(x_lock, y_lock):
-            print('Thread-2')
+        with acquire(y_lock):
+            with acquire(x_lock):
+                print('Thread-2')
 
 
 t1 = threading.Thread(target=thread_1)
@@ -47,3 +61,19 @@ t1.start()
 t2 = threading.Thread(target=thread_2)
 t2.daemon = True
 t2.start()
+
+
+def philosopher(left, right):
+    while True:
+        with acquire(left, right):
+            print(threading.current_thread(), 'eating')
+
+
+NSTICKS = 5
+chopsticks = [threading.Lock() for n in range(NSTICKS)]
+
+
+for n in range(NSTICKS):
+    t = threading.Thread(target=philosopher,
+                         args=(chopsticks[n], chopsticks[(n+1) % NSTICKS]))
+    t.start()
