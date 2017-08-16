8158b20f63e22a06e8f2bed3294d15f12d6aaf64
alonsovau <alonsovau@outlook.com>
2017-08-17 00:47:45 +0800

    2017-8-17

--- chapter12/st12n.py
+++ chapter12/st12n.py
@@ -1,13 +1,15 @@
 # 使用生成器来实现一个并发网络应用程序
 from collections import deque
 from select import select
+from socket import socket, AF_INET, SOCK_STREAM
+import time
 
 
 class YieldEvent:
     def handle_yield(self, sched, task):
         pass
 
-    def handle_resume(self, sche, task):
+    def handle_resume(self, sched, task):
         pass
 
 
@@ -53,3 +55,102 @@ class Scheduler:
                     raise RuntimeError('unrecognized yield event')
             except StopIteration:
                 self._numtasks -= 1
+
+
+class ReadSocket(YieldEvent):
+    def __init__(self, sock, nbytes):
+        self.sock = sock
+        self.nbytes = nbytes
+
+    def handle_yield(self, sched, task):
+        sched._read_wait(self.sock.fileno(), self, task)
+
+    def handle_resume(self, sched, task):
+        data = self.sock.recv(self.nbytes)
+        sched.add_ready(task, data)
+
+
+class WriteSocket(YieldEvent):
+    def __init__(self, sock, data):
+        self.sock = sock
+        self.data = data
+
+    def handle_yield(self, sched, task):
+        sched._write_wait(self.sock.fileno(), self, task)
+
+    def handle_resume(self, sched, task):
+        nsent = self.sock.send(self.data)
+        sched.add_ready(task, nsent)
+
+
+class AcceptSocket(YieldEvent):
+    def __init__(self, sock):
+        self.sock = sock
+
+    def handle_yield(self, sched, task):
+        sched._read_wait(self.sock.fileno(), self, task)
+
+    def handle_resume(self, sched, task):
+        r = self.sock.accept()
+        sched.add_ready(task, r)
+
+
+class Socket(object):
+    def __init__(self, sock):
+        self._sock = sock
+
+    def recv(self, maxbytes):
+        return ReadSocket(self._sock, maxbytes)
+
+    def send(self, data):
+        return WriteSocket(self._sock, data)
+
+    def accept(self):
+        return AcceptSocket(self._sock)
+
+    def __getattr__(self, item):
+        return getattr(self._sock, item)
+
+
+if __name__ == '__main__':
+    def readline(sock):
+        chars = []
+        while True:
+            c= yield sock.recv()
+            if not c:
+                break
+            chars.append(c)
+            if c ==b'\n':
+                break
+        return b''.join(chars)
+
+
+    class EchoServer:
+        def __init__(self, addr, sched):
+            self.sched = sched
+            sched.new(self.server_loop(addr))
+
+        def server_loop(self, addr):
+            s = Socket(socket(AF_INET, SOCK_STREAM))
+            s.bind(addr)
+            s.listen(5)
+            while True:
+                c, a = yield s.accept()
+                print('Got connection from', a)
+                self.sched.new(self.client_handler(Socket(c)))
+
+        def client_handler(self, client):
+            while True:
+                line = yield from readline(client)
+                if not line:
+                    break
+                line = b'Got ' + line
+                while line:
+                    nsent = yield client.send(line)
+                    line = line[nsent:]
+            client.close()
+
+        sched = Scheduler()
+        EchoServer(('', 16000), sched)
+        sched.run()
+
