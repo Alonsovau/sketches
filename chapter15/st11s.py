87d01c036a47b39af5e5b5f6e25cb399ab399dd0
alonsovau <alonsovau@outlook.com>
2017-09-13 23:54:51 +0800

    2017-9-13

--- chapter15/st11s.py
+++ chapter15/st11s.py
@@ -21,3 +21,7 @@ print(timeit.timeit('numpy.clip(b, -5, 5, c)', 'from __main__ import b, c, numpy
 print(timeit.timeit('sample11.clip(b, -5, 5, c)', 'from __main__ import b, c, sample11', number=1000))
 # 书上运行结果是numpy快，因为numpy核心代码是c
 # 而我这边是sample11快
+
+# @cython.boundscheck(False
+# @cython.wraparound(False)
+# 省去数组越界检查、消除相对数组的负数下标处理，可以极大提升性能
---
+++ chapter15/st12.py
@@ -0,0 +1,14 @@
+# 将函数指针转换为可调用对象
+import ctypes
+from llvm
+
+lib = ctypes.cdll.LoadLibrary(None)
+addr = ctypes.cast(lib.sin, ctypes.c_void_p).value
+print(addr)
+
+functype = ctypes.CFUNCTYPE(ctypes.c_double, ctypes.c_double)
+func = functype(addr)
+print(func)
+
+print(func(2))
+print(func(0))
