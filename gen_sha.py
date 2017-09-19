a892f1c374284df56a213d2674b92bccfb0967cb
alonsovau <alonsovau@outlook.com>
2017-09-19 23:59:29 +0800

    2017-9-19

--- gen_sha.py
+++ gen_sha.py
@@ -1,6 +1,8 @@
 import cx_Oracle
 import hashlib
 import json
+import subprocess
+import os
 from configparser import ConfigParser
 
 
@@ -37,6 +39,7 @@ def get_tables(username, password, address, database):
 
 def gen_exp(json_data, username, password, address, database):
     tables = json.loads(json_data)
+    exps = []
     for owner, tables in tables.items():
         for table in tables:
             conn = cx_Oracle.connect(username, password, address + '/' + database)
@@ -53,11 +56,18 @@ def gen_exp(json_data, username, password, address, database):
             for column_name, in cur:
                 mylist.append(column_name)
             mylist = mylist[:len(mylist)//2 + 1]
+            query = "query=\"order by " + ', '.join(mylist) + "\""
+            exp = "exp " + username + "/" + password + "" + query
+            exps.append(exp)
             print(mylist)
+    return exps
 
 
-
-
+def exc_exp(database, exps):
+    if not os.path.exists('./' + database):
+        os.mkdir('./' + database)
+    for exp in exps:
+        out_bytes = subprocess.check_output(exp, shell=True)
 
 
 if __name__ == '__main__':
@@ -68,9 +78,11 @@ if __name__ == '__main__':
     caddress = cfg.get('server', 'address')
     cdatabase = cfg.get('server', 'database')
     databases = cdatabase.split(',')
-    get_tables(cusername, cpassword, caddress, databases[0])
-    with open('orabiz_tables.json') as f:
-        myjsondata = f.read()
-        gen_exp(myjsondata, cusername, cpassword, caddress, databases[0])
-
+    # get_tables(cusername, cpassword, caddress, databases[0])
+    # with open('orabiz_tables.json') as f:
+    #     myjsondata = f.read()
+    #     gen_exp(myjsondata, cusername, cpassword, caddress, databases[0])
+    mylist = ['id1', 'id2', 'id3']
+    query = "query=\"order by " + ', '.join(mylist) + "\""
+    print(query)
 
