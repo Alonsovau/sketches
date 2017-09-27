cb04fd539045b8e1a2fbd94bbcc152df2d103bf6
alonsovau <alonsovau@outlook.com>
2017-09-27 23:56:23 +0800

    2017-9-27

--- exec_shell.py
+++ exec_shell.py
@@ -2,15 +2,18 @@ import subprocess
 import os
 
 
-fd = open('stdout.txt', 'w')
-with subprocess.Popen(["ls", "-l"], shell=False, stdout=fd, close_fds=True) as proc:
-    # print(proc.stdout.read())
-    pass
-out = subprocess.check_output('ls -l', shell=True)
-print(out)
-
+# fd = open('stdout.txt', 'w')
+# with subprocess.Popen(["exp"], shell=True, stdout=subprocess.PIPE, close_fds=True) as proc:
+#     # print(proc.stdout.read())
+#     pass
+# out = subprocess.check_output('ls -l', shell=True)
+# print(out)
+#
 command = "python3 " + os.path.dirname(os.path.realpath(__file__)) + "/ss.py"
 print(command)
 sub = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
-sub.wait()
-print(sub.poll())
+# rout = sub.stdout.readline()
+while sub.returncode is None:
+    rout = sub.stdout.readline()
+    print(rout)
+    sub.wait()
