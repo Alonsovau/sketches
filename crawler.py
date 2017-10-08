132df71193037e84b86b819158cf18d5235052c9
alonsovau <alonsovau@outlook.com>
2017-10-09 01:07:22 +0800

    2017-10-9

--- crawler.py
+++ crawler.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+import time
 
 
 browser = webdriver.Chrome(executable_path='/Users/zhouxin/Downloads/chromedriver')
@@ -15,10 +18,19 @@ print(username.get_attribute('placeholder'))
 username.send_keys('1017092687@qq.com')
 passwd = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/'
                                        'div[2]/div/input')
-passwd.send_keys('wasd123')
+passwd.send_keys('wasd123123')
 login = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/span/button')
 login.click()
-print(browser.find_element_by_id('slfErrorAlert'))
+time.sleep(1)
+login_status = 0
+try:
+    print(browser.find_element_by_id('slfErrorAlert').text)
+except NoSuchElementException:
+    print('login ok')
+    login_status = 1
+if login_status == 1:
+    piclist = browser.find_elements_by_xpath('//*[@id="mainFeed"]/div/div/div[1]/div/article[4]/div[1]/div/div/div[2]')
+    print(piclist[0])
 with open('ins.html', 'w') as f:
     f.write(browser.page_source)
 # browser.quit()
